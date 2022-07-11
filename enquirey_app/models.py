927d8204be007491356a3400a7762da3d0ca0177
jenish-codes <jenish.codes@gmail.com>
2022-07-12 01:40:09 +0530

    migration 0001
    -first migration -created models tabel

--- enquirey_app/models.py
+++ enquirey_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Enquiry(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=30)
+    message = models.CharField(max_length=300)
--- enquirey_app/views.py
+++ enquirey_app/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 
 def enquiryForm(request):
-    return render(request, 'enquiry_app/enquiry.html')
+    if request.method == 'POST':
+        # name = request.POST.get['name']
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+    return render(request, "enquiry_app/enquiry.html")
+
+# def enquiryDetails(request):
+#     return render(request, template_name)
